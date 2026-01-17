7f176d2c1c678268e89ff03effdb6a055f7e9d3c
Rann30 <zafran.jangkung30@gmail.com>
2026-01-17 20:13:41 +0700

    Update routing dan controller PostController

---
+++ resources/views/posts/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>tentang saya</title>
+</head>
+
+<body>
+    <h1>about</h1>
+    <p>saya zaffran ganteng</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SekolahController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Basic Route
+Route::get('/hello', function () {
+    return 'Selamat Datang di Praktik Laravel 12!';
 });
+
+// Route dengan Parameter (Required)
+Route::get('/user/{id}', function ($id) {
+    return 'Profil Pengguna dengan ID: ' . $id;
+});
+
+// Route dengan Parameter Opsional
+Route::get('/kategori/{slug?}', function ($slug = 'umum') {
+    return 'Menampilkan Artikel Kategori: ' . $slug;
+});
+
+// Named Route
+Route::get('/dashboard/statistik/pengunjung', function () {
+    return 'Halaman Statistik Pengunjung';
+})->name('admin.statistik');
+
+// Redirect Route
+Route::redirect('/tentang-kami', '/about');
+
+Route::get('/about', function () {
+    return 'Ini adalah Halaman Tentang Kami yang baru';
+});
+
+// Route menggunakan Controller
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
